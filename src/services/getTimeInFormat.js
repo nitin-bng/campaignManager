2047cc4056a2bf37a231900d21b718be74035193
Nitin <nitin.kalra@blackngreen.com>
2022-09-14 13:56:40 +0530

    validation on config page

--- src/services/getTimeInFormat.js
+++ src/services/getTimeInFormat.js
@@ -1,10 +1,15 @@
 const changeTimeFormatForBackend = (date) =>{ 
-    let result = ''
-    result += date.getHours().toString().length >= 2 ? date.getHours() : '0' + date.getHours()
-    result += ':'
-    result += date.getMinutes().toString().length >= 2 ? date.getMinutes() : '0' + date.getMinutes();
-    result += ':00'
-    return result
+    if(date){
+        let result = ''
+        result += date.getHours().toString().length >= 2 ? date.getHours() : '0' + date.getHours()
+        result += ':'
+        result += date.getMinutes().toString().length >= 2 ? date.getMinutes() : '0' + date.getMinutes();
+        result += ':00'
+        return result
+    }
+    else{
+        return date
+    }
 }
 
 String.prototype.replaceAt = function(index, replacement) {
@@ -12,22 +17,27 @@ String.prototype.replaceAt = function(index, replacement) {
 }
 
 const changeTimeFormatForFrontend = time =>{
-    let date = new Date().toString()
-    let result = ''
-    let currPosition = 0
+   if(time){ 
+        let date = new Date().toString()
+        let result = ''
+        let currPosition = 0
 
-    for(let i=0; i<date.length; i++){
-        console.log('nitin function loop', date[i], result)
-        result += date[i]
-        if(date[i] === ':'){
-            result = result.replaceAt(i-2, [time[currPosition]])
-            currPosition++
-            result = result.replaceAt(i-1, [time[currPosition]])
-            currPosition++
-            currPosition++
+        for(let i=0; i<date.length; i++){
+            console.log('nitin function loop', date[i], result)
+            result += date[i]
+            if(date[i] === ':'){
+                result = result.replaceAt(i-2, [time[currPosition]])
+                currPosition++
+                result = result.replaceAt(i-1, [time[currPosition]])
+                currPosition++
+                currPosition++
+            }
         }
+        return result
     }
-    return result
+    else{
+        return time
+    }
 } 
 
 export {changeTimeFormatForFrontend, changeTimeFormatForBackend}
